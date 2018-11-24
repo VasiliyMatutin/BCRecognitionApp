6bf69cd3a541643b2c6c099c609ddfd43c10617d
Vasiliy Matutin <vmatutin@gmail.com>
2018-11-24 21:52:41 +0300

    Initial commit

---
+++ app/src/main/java/com/hse/vasiliy/bcrecognition/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hse.vasiliy.bcrecognition
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
